e3b368fe2d6b9bb1ed5abe6f05637e74c8d6f06c
Goziri <goziri.dev@gmail.com>
2026-08-21 04:19:32 +0100

    Remove: longest-common-prefix - submission-11

--- Data Structures & Algorithms/longest-common-prefix/submission-11.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        longest_prefix = ""
-        for i, char in enumerate(strs[0]):
-            for j in range(len(strs)):
-                if len(strs[j]) - 1 < i or char != strs[j][i]:
-                    return longest_prefix
-            longest_prefix += char
-        return longest_prefix
